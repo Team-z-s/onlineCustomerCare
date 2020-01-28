1cf9fb343042c9b19a0a9a4f7f3fb3a2cf872721
nahilishasho <shashonahli702@gmail.com>
2020-01-28 11:05:40 -0800

    company admin handler

--- go/src/github.com/online-service-and-customer-care/entity/entities.go
+++ go/src/github.com/online-service-and-customer-care/entity/entities.go
@@ -1,25 +1,36 @@
 package entity
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"time"
 )
 
-//Admin stract containing admin info
+
+
+var JwtKey = []byte("my_secret_key")
+
+type Claims struct {
+	Username       string `json:"username"`
+	jwt.StandardClaims
+}
+
+//Admin struct contain admin info
 type Admin struct {
 	username string
 	password string
 }
 
-//User struct containing users
+//User struct contain info about user
 type User struct {
-	ID    int    `json: "id"`
-	FName string `json: "fname" gorm:"type:varchar(255); not null"`
-	LName string `json: "lname" gorm:"type:varchar(255); not null"`
-	Email string `json: "email" gorm:"type:varchar(255);unique"`
-	Phone string `json: "phone" gorm:"type:varchar(255)"`
 
-	Username string `json: "username" gorm:"type:varchar(255); not null; unique"`
-	Password string `json: "password" gorm:"type:varchar(255)"`
+	ID int
+	FName string `gorm:"type:varchar(255); not null"`
+	LName string `gorm:"type:varchar(255); not null"`
+	Email string `gorm:"type:varchar(255);unique"`
+	Phone string `gorm:"type:varchar(255)"`
+
+	Username string `gorm:"type:varchar(255); not null; unique"`
+	Password string `gorm:"type:varchar(255); not null"`
 }
 
 //Address contains country,city, woreda,kebele
@@ -29,38 +40,39 @@ type Address struct {
 
 //Employee struct contains employees
 type Employee struct {
-	ID        int    `json:"id"`
-	companyID int    `json:"company_id"`
-	FName     string `json: "fname" gorm:"type:varchar(255)" `
-	LName     string `json: "lname" gorm:"type:varchar(255)"`
-	Email     string `json:"email" gorm:"type:varchar(255);not null; unique"`
-
-	Address  string  `json: "address"`
-	Sallary  float64 `json: "sallary"`
-	Phone    string  `json: "phone" gorm:"type:varchar(255)"`
-	photo    string  `json: "photo" gorm:"type:varchar(255)"`
-	Username string  `json: "username" gorm:"type:varchar(255);not null; unique"`
-	Password string  `json: "password" gorm:"type:varchar(255) "`
+	EmployeeID        int
+	CompanyID int
+	company Companie
+	FName     string  `gorm:"type:varchar(255)" `
+	LName     string  `gorm:"type:varchar(255)"`
+	Email     string  `gorm:"type:varchar(255);not null; unique"`
+	Address   string
+	Salary    float64
+	Phone     string  `gorm:"type:varchar(255)"`
+	Photo     string  `gorm:"type:varchar(255)"`
+	Username  string  `gorm:"type:varchar(255);not null; unique"`
+	Password  string  `gorm:"type:varchar(255); not null "`
 }
 
 //Company struct contain fields of the company
 type Companie struct {
-	ID       int    `json: "id"`
-	FullName string `json: "fullname"`
-	Logo     string `json: "logo"`
-	Email    string `json: "email"`
-	Phone    string `json: "phone"`
-	Address  string `json: "address"`
-	Moto     string `json: "moto"`
-	Password string `json: "password" gorm:"type:varchar(255) "`
+	CompanyID       int
+	FullName string `gorm:"type:varchar(255);not null; unique"`
+	Logo     string
+	Email    string `gorm:"type:varchar(255);not null; unique"`
+	Phone    string `gorm:"type:varchar(255);not null; unique"`
+	Address  string
+	Moto     string
+	Password string `gorm:"type:varchar(255);not null "`
 }
 
 //Service contains fields of service
 type Service struct {
-	ID          int    `json: "id"`
-	CompanyID   int    `json: "company_id"`
-	Name        string `json: "service_name"`
-	Description string `json: "description"`
+	ID          int
+	CompanyID   int
+	Company Companie
+	Name        string `gorm:"type:varchar(255);not null "`
+	Description string
 }
 
 //Date c
@@ -72,31 +84,31 @@ type Date struct {
 
 //Attendance contain date and time
 type Attendance struct {
-	ID         int       `json: "id"`
-	EmployeeID int       `json: "employee_id"`
-	Shift      string    `json: "shift"`
-	Date       Date      `json: "data_a"`
-	Status     string    `json: "status"`
-	postedAt   time.Time `json: "posted_at"`
+	ID         int       `json:"id"`
+	EmployeeID int       `json:"employee_id"`
+	Shift      string    `json:"shift"`
+	Date       Date      `json:"data_a"`
+	Status     string    `json:"status"`
+	PostedAt   time.Time `json:"posted_at"`
 }
 
-//Comment coment
+//Comment comment
 type Comment struct {
-	ID       uint   `json:"id"`
-	username string `json:"usename" gorm:"type:varchar(255)"`
-	Message  string `json:"message"`
-	//Phone    string    `json:"phone" gorm:"type:varchar(100);not null; unique"`
-	Email    string    `json:"email" gorm:"type:varchar(255);not null; unique"`
-	PlacedAt time.Time `json:"posted_at"`
+	ID        uint
+	FullName  string `gorm:"type:varchar(255)"`
+	Message   string
+	Phone     string `gorm:"type:varchar(100);not null; unique"`
+	Email     string `gorm:"type:varchar(255);not null; unique"`
+	CreatedAt time.Time
 }
-
 //Jobs employee's work
 type Employee_job struct {
-	EmployeeID int     `json: "employee_id"`
-	CompanyID  int     `json: "company_id"`
-	Job        string  `json: "job"`
-	DayRate    float64 `json: "day_rate"`
-	NightRate  float64 `json: "night_rate"`
+	EmployeeID int
+	CompanyID  int
+	Employee Employee
+	Job        string  `json:"job"`
+	DayRate    float64 `json:"day_rate"`
+	NightRate  float64 `json:"night_rate"`
 }
 
 //Roles role
@@ -105,11 +117,28 @@ type Roles struct {
 	Name string
 }
 
+type Account struct{
+	Username string `gorm:"type:varchar(255);not null;unique"`
+	Password string `gorm:"type:varchar(255);not null"`
+
+	Role_id int `json:"role_id" `
+}
+
 //Task taskes assigned for the employee
+
 type Task struct {
-	ID          int    `json: "id"`
-	EmployeeID  int    `json: "employee_id"`
-	Name        string `json: "task_name"`
-	Description string `json: "description"`
-	progress    string `json: "progress"`
+	ID          int
+	EmployeeID  int
+	Employee Employee
+	Name        string `gorm:"type:varchar(255);not null"`
+	Description string
+	Progress    string `gorm:"type:varchar(255);"`
+}
+//Session represents login user session
+type Session struct {
+	//ID         uint
+	//Uername    string `gorm:"type:varchar(255);not null"`
+	//UUID       string `gorm:"type:varchar(255);not null"`
+	//Expires    int64  `gorm:"type:varchar(255);not null"`
+	SigningKey []byte `gorm:"type:varchar(255);not null"`
 }
